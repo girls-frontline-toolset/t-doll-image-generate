27073f0848c625ac87d75597f5d98a30e88ac0f2
andy19974a22 <andy19974a22@gmail.com>
2019-03-17 21:02:04 +0800

    docs(list): add t-Doll list and Ex t-doll list

--- modules/Resource.mjs
+++ modules/Resource.mjs
@@ -24,14 +24,26 @@
  * @property {DollType} type
  * @property {string} src
  */
-import request from  'sync-request'
+import request from  'sync-request';
+import tDoll from './t-doll.json';
+import exTDoll from './ex-t-doll.json';
+
 // const hostName = 'https://www.ntw-20.com/';
 const hostName = 'http://127.0.0.1/';
+const isAPI = false;
 
 /** @type {t-doll[]} */
-let tDollList = JSON.parse(request('GET', hostName + '/api/inquiry/allGirl').getBody('utf8')).data;
-/** @type {ex-t-Doll} */
-let exTDollList = JSON.parse(request('GET', hostName + '/api/inquiry/allExGirl').getBody('utf8')).data;
+let tDollList = null;
+/** @type {ex-t-Doll[]} */
+let exTDollList = null;
+
+if (isAPI) {
+    tDollList = JSON.parse(request('GET', hostName + '/api/inquiry/allGirl').getBody('utf8')).data;
+    exTDollList = JSON.parse(request('GET', hostName + '/api/inquiry/allExGirl').getBody('utf8')).data;
+} else {
+    tDollList = tDoll;
+    exTDollList = exTDoll;
+}
 
 export {tDollList, exTDollList};
 
